88f5c3bfb9e1eb936f93e5e56be79708b170515b
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-09-03 14:35:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,6 +73,3 @@ SELF_DELETE = environ.get('SELF_DELETE', False)
 if SELF_DELETE == "True":
     SELF_DELETE = True
 
-class Tamilgram(object):
-         TUTORIAL_VIDEO = environ.get('TUTORIAL_VIDEO', 'https://t.me/admin')
-         URL_BUTTON = environ.get('URL_BUTTON', 'https://t.me/TamilGram_Official')
