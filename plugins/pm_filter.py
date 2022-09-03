89fcc8b2f3b42192705abff80b729790eabbdf8f
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-09-03 14:37:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,7 @@ import ast
 import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
-from Tamilgram import Tamilgram
+from Tamilgram import tamilgram
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
