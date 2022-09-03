73cd439b1f47fde686b1f2bb66c82c09ffea736b
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-09-03 14:41:25 +0530

    Update Tamilgram.py

--- Tamilgram.py
+++ Tamilgram.py
@@ -1,4 +1,6 @@
+Import os
+
 class tamilgram(object):
 
-TUTORIAL_VIDEO = environ.get('TUTORIAL_VIDEO', 'https://t.me/admin')
-URL_BUTTON = environ.get('URL_BUTTON', 'https://t.me/TamilGram_Official')
+TUTORIAL_VIDEO = environ.get('TUTORIAL_VIDEO', "https://t.me/admin")
+URL_BUTTON = environ.get('URL_BUTTON', "https://t.me/TamilGram_Official")
