a763edd0d26069d3d1c7ec0ceca513c0b37f9d64
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-09-03 14:52:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,16 @@
 #### This Code Was Devloped By Tamilgram ####
 
-import os
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
