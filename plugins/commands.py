6c28c0cc6f6448512cd372730ef55b33aab6ac1e
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-09-05 19:02:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,13 +243,13 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-        await client.send_cached_media(
+    await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text=CAPTION_BUTTON,url=CAPTION_BUTTON_URL) ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
+                    
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
